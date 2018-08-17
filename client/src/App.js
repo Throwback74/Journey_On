22e819919927aad2de5c06503effcebab96a95e8
Throwback74 <corey.slade@gmail.com>
2018-08-17 13:30:37 -0700

    update with boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AuthService from './components/AuthService';
+import withAuth from './components/withAuth';
+const Auth = new AuthService();
 
 class App extends Component {
+
+  state = {
+    userId: this.props.user.id,
+    profileLink: ""
+  };
+
+  componentDidMount() {
+    const profileLinkURL = `/profile/${this.state.userId}`;
+    this.setState({
+      profileLink: profileLinkURL
+    });
+  }
+
+  handleLogout = () => {
+    Auth.logout();
+    this.props.history.replace('/signup');
+  };
+
+  goToEditProfile = () => {
+    this.props.history.replace(this.state.profileLink);
+  };
+
   render() {
+    console.log(process.env.REACT_APP_SECRET_CODE);
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome {this.props.user.email}</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button type="button" className="btn btn-primary" onClick={this.goToEditProfile}>Go to Profile</button>
+          <button type="button" className="btn btn-danger" onClick={this.handleLogout}>Logout</button>
         </p>
       </div>
     );
   }
 }
 
-export default App;
+export default withAuth(App);
