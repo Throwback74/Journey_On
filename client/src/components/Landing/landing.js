55d896f5bc97f6befef0ab6d5d640af58eab7fdd
wesley9mm <info.developer4@gmail.com>
2018-08-21 13:14:09 -0700

    added grid and containers to landing page

--- client/src/components/Landing/landing.js
+++ client/src/components/Landing/landing.js
@@ -7,27 +7,41 @@ const Landing = props => (
 
 <div>
 <header>
+  <div className="container">
   <Link to="/signup">
     <button type="button" className="btn btn-primary">Signup</button>
   </Link>
   <Link to="/login">
     <button type="button" className="btn btn-danger">Login</button>
   </Link>
-    <h1>JOURNEY ON</h1>
-    <p className="paragraph">Wirize is a ready-made checkout payment built on Stripe. <p>Collect one-time payments and sell your digital services
+ 
+    <h1>JOURNEY ON</h1> 
+    <div className="row">
+      <div className="col s8">
+        <p className="paragraph">Wirize is a ready-made checkout payment built on Stripe. <p>Collect one-time payments and sell your digital services
   in minutes</p> <p>(no code or website required)</p></p>
+      </div>
+      <div className="col s4">
+        <div>
+          <img className="picture" src="https://via.placeholder.com/414x396" alt="image"/>
+        </div>
+      </div>
+    </div>
+    </div>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
     <polygon fill="white" points="0,100 100,0 100,100"/>
     </svg>  
+    
 </header>
 
 <section>
+  <div className="container">
   <h2>Section Content</h2>
   <div className="row">
-      <div className="col s3">
-            (PICTURE)
+      <div className="col s6">
+      <a href="https://placeholder.com"><img src="https://via.placeholder.com/414x396"/></a>
       </div>
-      <div className="col s3">
+      <div className="col s6">
       <h1>Get Started</h1>
       Link your journey account by connecting an existing or new social media account.
       <ul>
@@ -37,7 +51,7 @@ const Landing = props => (
       </div>
     </div>
   <div className="row">
-      <div className="col s3">
+      <div className="col s6">
       <h1>Fresh Start</h1>
       Whether you collect client payments, or sell digital services online,Wirize has you covered.
       <ul>
@@ -45,15 +59,15 @@ const Landing = props => (
         <li>Pay only for what you use</li>
       </ul>
       </div>
-      <div className="col s3">
-              (PICTURE)
+      <div className="col s6">
+      <a href="https://placeholder.com"><img src="https://via.placeholder.com/414x396"/></a>
       </div>
     </div>
   <div className="row">
-      <div className="col s4">
-              (PICTURE)
+      <div className="col s6">
+      <a href="https://placeholder.com"><img src="https://via.placeholder.com/414x396"/></a>
       </div>
-      <div className="col s4">
+      <div className="col s6">
       <h1>Completed Goal</h1>
       Whether you collect client payments, or sell digital services online,Wirize has you covered.
       <ul>
@@ -62,12 +76,12 @@ const Landing = props => (
       </ul>
       </div>
     </div>
+</div>
   <footer>
     <div className="foot">
     <h3>Footer</h3>
     </div>
   </footer>
-
 </section>
 </div>
 
