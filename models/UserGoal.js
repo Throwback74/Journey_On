8b1ec692675311cb7070410f406cd3f7065fd9d0
Nick Woodward <nickwoodward5@gmail.com>
2018-08-21 14:19:45 -0700

    populating goals for each user

--- models/UserGoal.js
+++ models/UserGoal.js
@@ -50,37 +50,11 @@ let UserGoal = new Schema({
     default: Date.now
   },
   completeBy: {
-      type: Date,
-      default: Date.now,
-      required: true
-  },
-
+    type: Date,
+    default: Date.now,
+    required: true
+  }
 });
 
-// Execute before each user.save() call
-UserGoal.pre('save', function(callback) {
-  let user = this;
-
-  // Break out if the password hasn't changed
-  if (!user.isModified('password')) return callback();
-
-  // Password changed so we need to hash it
-  bcrypt.genSalt(5, function(err, salt) {
-    if (err) return callback(err);
-
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) return callback(err);
-      user.password = hash;
-      callback();
-    });
-  });
-});
-
-UserGoal.methods.verifyPassword = function(password, cb) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
 
 module.exports = mongoose.model('UserGoal', UserGoal);
--- server.js
+++ server.js
@@ -58,14 +58,28 @@ app.post('/api/signup', (req, res) => {
 // ADD GOAL ROUTE
 app.post('/api/addgoal', (req, res) => {
   db.UserGoal.create(req.body)
-    .then(data => res.json(data))
-    .catch(err => res.status(400).json(err));
+    .then(function(dbGoals) {
+      // If a Book was created successfully, find one library (there's only one) and push the new Book's _id to the Library's `books` array
+      // { new: true } tells the query that we want it to return the updated Library -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.User.findOneAndUpdate({_id: req.body.UserId}, { $push: { goals: dbGoals._id } }, { new: true });
+    })
+    .then(function(dbUser) {
+      // If the Library was updated successfully, send it back to the client
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+      // If an error occurs, send it back to the client
+      res.json(err);
+    });
 });
 
 // Any route with isAuthenticated is protected and you need a valid token
 // to access
 app.get('/api/user/:id', isAuthenticated, (req, res) => {
-  db.User.findById(req.params.id).then(data => {
+  db.User.findById(req.params.id)
+  .populate("goals")
+  .then(data => {
     if(data) {
       res.json(data);
     } else {
