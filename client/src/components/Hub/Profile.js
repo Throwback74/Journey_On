67b92a89aa32a77c5b2f9cbf00b377a68c56c6d9
Etch <Etch.rush@gmail.com>
2018-08-21 19:42:54 -0800

    icons added

--- client/src/components/Hub/Profile.js
+++ client/src/components/Hub/Profile.js
@@ -4,6 +4,7 @@ import API from '../../utils/API';
 import { Link } from 'react-router-dom';
 import "../Hub/Home.css";
 
+
 class Profile extends Component {
 
   state = {
@@ -36,7 +37,19 @@ class Profile extends Component {
             <h1>Welcome... {this.state.username}</h1>
             <p>Time to get shit done!</p>
           </div>
-
+          <div className="container">
+            <div className="middle row">
+              <div className="icon1 col-md-4">
+              <button className="button button1"></button>
+            </div>
+              <div className="icon2 col-md-4">
+              <button className="button button2"></button>
+            </div>
+              <div className="icon3 col-md-4">
+              <button className="button button3"></button>
+            </div>
+            </div>
+          </div>
           <div className="container progress">
             <h1 className="container bar">Progress Bar</h1>
           </div>
