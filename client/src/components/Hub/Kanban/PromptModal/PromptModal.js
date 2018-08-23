fbf56f003e175eab16c528b3ea6221f19452b61b
mdamian9 <m.damian9@hotmail.com>
2018-08-23 00:50:53 -0700

    Updated modal to show new task prompt for user, added some CSS

--- client/src/components/Hub/Kanban/PromptModal/PromptModal.js
+++ client/src/components/Hub/Kanban/PromptModal/PromptModal.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Modal from 'react-modal';
+import './PromptModal.css'
 
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
   }
 };
 
@@ -29,7 +30,7 @@ class PromptModal extends React.Component {
   }
 
   openModal() {
-    this.setState({modalIsOpen: true});
+    this.setState({ modalIsOpen: true });
   }
 
   afterOpenModal() {
@@ -38,7 +39,7 @@ class PromptModal extends React.Component {
   }
 
   closeModal() {
-    this.setState({modalIsOpen: false});
+    this.setState({ modalIsOpen: false });
   }
 
   render() {
@@ -53,17 +54,23 @@ class PromptModal extends React.Component {
           style={customStyles}
           contentLabel="Example Modal"
         >
-
-          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-          <button onClick={this.closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
+          <div className="text-center">
+            <h2 ref={subtitle => this.subtitle = subtitle}>Get Started</h2>
+            <b>Create a new task!</b><br></br>
+            <form id="task-form">
+              {/* <input />
             <button>tab navigation</button>
             <button>stays</button>
             <button>inside</button>
-            <button>the modal</button>
-          </form>
+            <button>the modal</button> */}
+              Title:<br></br>
+              <input type="text" name="task-title" id="task-title"></input><br></br>
+              Description:<br></br>
+              <textarea rows="4" cols="20" name="task-description" id="task-descriptions" form="task-form"></textarea><br></br>
+              <input type="submit" value="Submit" id="submit-task-btn"></input>
+            </form>
+            <button onClick={this.closeModal}>close</button>
+          </div>
         </Modal>
       </div>
     );
